ff1b991716517553393316f8f23bd5009d9866c7
Di Huang <39016210+qwe1256@users.noreply.github.com>
2023-09-13 22:39:39 -0700

    Update training.py

--- modules/training.py
+++ modules/training.py
@@ -436,9 +436,9 @@ def do_train(lora_name: str, always_override: bool, save_steps: int, micro_batch
 
         def generate_prompt(data_point: dict[str, str]):
             for options, data in format_data.items():
-                if set(options.split(',')) == set(x[0] for x in data_point.items() if (type(x[1]) is str and len(x[1].strip()) > 0)):
+                if set(options.split(',')) == set([x[0] for x in data_point.items() if ((x[1]) is not None and len(x[1].strip()) > 0)][0].split(',')):
                     for key, val in data_point.items():
-                        if type(val) is str:
+                        if val is not None:
                             data = data.replace(f'%{key}%', val)
                     return data
             raise RuntimeError(f'Data-point "{data_point}" has no keyset match within format "{list(format_data.keys())}"')
